c30dea2b8d7fb09cbde44b52572da1bf4efde347
Manuel <mgordillo11@outlook.com>
2022-03-09 21:33:50 -0700

    Started CP3 Project

---
+++ anime-search/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+
+    <div class="github-container">
+      <ul class="social-links">
+        <li>
+          <a
+            href="https://github.com/mgordillo11/CS260-Creative-Project-3"
+            target="blank"
+            ><i class="fab fa-github"></i
+          ></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.github-container {
+  display: flex;
+  justify-content: center;
+  font-size: 100px;
+}
+
+.fab {
+  color: black;
+}
+
+.github-container li {
+  list-style: none;
+}
+</style>
